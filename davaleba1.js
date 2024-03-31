12f50d9159c35e135196963909b230aee8ded80e
saba markozashvili <162030954+sabamarkozashvili@users.noreply.github.com>
2024-03-31 22:46:38 +0400

    Add files via upload

---
+++ davaleba1.js
@@ -0,0 +1,16 @@
+var arr =["qaco","kobrebi","boku","markoza"]
+function compae(){
+    var found = false;
+    for(var i of arr){
+        if(i == inp1value){
+            found =true;
+            break;
+        }
+    }
+}
+if(found ==true){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
